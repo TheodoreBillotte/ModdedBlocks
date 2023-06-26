2b19f4426b2bf8a2e3581da0651b668ae6628cef
TheodoreBillotte <theodore.billotte@epitech.eu>
2023-06-26 21:53:37 +0200

    add: packet manager use reflection for multi version compatibility

--- src/main/java/fr/theobosse/moddedblocks/managers/PacketManager.java
+++ src/main/java/fr/theobosse/moddedblocks/managers/PacketManager.java
@@ -1,29 +1,64 @@
 package fr.theobosse.moddedblocks.managers;
 
+import fr.theobosse.moddedblocks.ModdedBlocks;
 import net.minecraft.network.protocol.Packet;
-import org.bukkit.craftbukkit.v1_19_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
+
+import java.lang.reflect.Method;
 
 public class PacketManager {
 
     private final Player player;
-    private final CraftPlayer craftPlayer;
+    private final Object playerConnection;
+    private final Method sendPacketMethod;
+    private final String nmsVersion;
 
     public PacketManager(Player player) {
         this.player = player;
-        this.craftPlayer = (CraftPlayer) player;
+        this.playerConnection = getPlayerConnection();
+        this.sendPacketMethod = getSendPacketMethod();
+        this.nmsVersion = getNMSVersion();
+    }
+
+    private String getNMSVersion(){
+        String v = ModdedBlocks.getInstance().getServer().getClass().getPackage().getName();
+        return v.substring(v.lastIndexOf('.') + 1);
+    }
+
+    private Object getPlayerConnection() {
+        try {
+            Object handleClass = player.getClass().getMethod("getHandle").invoke(player);
+            return handleClass.getClass().getField("playerConnection").get(handleClass);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private Method getSendPacketMethod() {
+        try {
+            return playerConnection.getClass().getMethod("sendPacket");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public Player getPlayer() {
         return player;
     }
 
-    public CraftPlayer getCraftPlayer() {
-        return craftPlayer;
+    public String getNmsVersion() {
+        return nmsVersion;
     }
 
     public void sendPacket(Packet<?> packet) {
-        craftPlayer.getHandle().b.a(packet);
+        if (sendPacketMethod == null || playerConnection == null) return;
+        try {
+            sendPacketMethod.invoke(playerConnection, packet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
